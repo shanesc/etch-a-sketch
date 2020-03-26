259da57048b8a9a03830889ad8992f7a37823411
Shane Mitchell <shaneSC@gmail.com>
2020-03-26 13:47:01 +0000

    Add coloring of squares when user mouses over

--- app.js
+++ app.js
@@ -5,5 +5,7 @@ const gridSize = 16;
 for (let i = 0; i < gridSize ** 2; i++) {
   let square = document.createElement('div');
   square.classList.add('item');
+  square.addEventListener('mouseenter', function() {square.classList.add('item--colored')});
   container.appendChild(square);
 }
+
